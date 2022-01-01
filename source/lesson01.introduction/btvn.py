03ede0d970df23ab663bb226b5d4e0deec820ea3
hoan.phamphu <hoan.pham@genefriendway.com>
2022-01-01 20:14:04 +0700

    Lesson 01

---
+++ source/lesson01.introduction/btvn.py
@@ -0,0 +1,10 @@
+""" BÀI TẬP VỀ NHÀ
+
+Bài 01. Code chương trình thực hiện các công việc sau:
+    1. Khai báo và gán giá trị cho 5 biến a, b, c, d, e
+    2. Tính tổng của 5 biến trên và in kết quả ra màn hình
+    3. Tính trung bình cộng của 5 biến trên và in kết quả ra màn hình
+    4. Tính tích của 5 biến trên và in kết quả ra màn hình
+    5. Tính giá trị biểu thức P = (a+b+c+d+e) / (a*b*c*d*e) và in kết quả ra màn hình
+
+"""
